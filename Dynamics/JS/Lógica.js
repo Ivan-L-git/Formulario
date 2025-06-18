3a06b36d95590696345e58d588d86f59fd05acc6
Ivan-L-git <324269440@alumno.enp.unam.mx>
2025-06-18 12:57:37 -0600

    Validación

--- Dynamics/JS/Lógica.js
+++ Dynamics/JS/Lógica.js
@@ -1,1 +1,21 @@
-let estas_seguro=document.getElementById("enviar");
+let Formulario=document.getElementById("Formulario");
+let nombre=document.getElementById("nombre");
+let codigo_postal=document.getElementById("Formulario");
+
+Formulario.addEventListener("submit", function(e){
+
+    if (nombre.value == "")
+    {
+        e.preventDefault();
+        alert("No ingresaste tu nombre");
+    }
+    console.log("Nombre del usuario:" + nombre.value);
+    let digitos_postal=codigo_postal.value;
+    if (codigo_postal.length!="5")
+    {
+        e.preventDefault();
+        alert("El código postal es incorrecto");
+    }
+    console.log("Código postal:" + codigo_postal);
+}
+);
